9984a24488eb97aaf5d6257cfae4f0e4d8bd9b3a
liziniu <374387855@qq.com>
2019-05-12 14:24:07 +0800

    update visz

--- runexp.sh
+++ runexp.sh
@@ -2,7 +2,7 @@ python run.py \
     --env maze-sample-5x5-v0 \
     --env_type maze \
     --num_env 1 \
-    --alg her2 \
+    --alg her \
     --num_timesteps 1e5 \
     --gpu 12,13,2 \
     --her 1.0 \
